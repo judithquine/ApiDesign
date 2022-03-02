20b9c142fc35f783c6cad4ff3e61261e55d91aa3
judithquine <judithquine@yahoo.es>
2022-03-02 18:34:26 +0100

    added methods add & subtract

--- src/main/java/com/rest/factoria/controllers/AddController.java
+++ src/main/java/com/rest/factoria/controllers/AddController.java
@@ -4,22 +4,30 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class AddController {
-    private static int total = 1;
 
-    @GetMapping("total")
-    public int Total(){
-        return total;
-    }
+    private static Integer totalValue = 1;
+
 
     @GetMapping("hello")
-    public String Hello(){
-        return "Hello";
+    public String Hello(String name, Integer age) {
+        return "Hello " + name + " " + age;
+    }
+
+    @GetMapping("total")
+    public Integer total() {
+        return totalValue;
     }
 
     @PostMapping("add")
-    public void Add(int valueToAdd){
-        total += valueToAdd;
+    public Integer add(Integer valueToAdd) {
+        return totalValue+=valueToAdd;
+    }
+
+    @PostMapping("subtraction")
+    public Integer subtraction(Integer valueToSubtract) {
+        return totalValue-=valueToSubtract;
     }
 }
